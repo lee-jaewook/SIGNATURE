5abf75ddcba671d1dca8e8385a639b31f0bd861e
lee-jaewook <jalee4930@gmail.com>
2021-05-24 14:44:18 +0900

    add stt

--- src/views/Stt.vue
+++ src/views/Stt.vue
@@ -5,12 +5,44 @@
 </template>
 
 <script>
-export default {
-  methods: {
-    onEnd ({ lastSentence, transcription }) {
-      // `lastSentence` is the last sentence before the pause
-      // `transcription` is the full array of sentences
-    }
+
+  export default {
+   name: 'speech_to_text',
+   data() {
+     return {
+       runtimeTranscription_: "",
+       transcription_: [],
+       lang_: "ko"
+     };
+   },
+   methods: {
+    startSpeechToTxt() {
+    // initialisation of voicereco
+    
+    window.SpeechRecognition =
+    window.SpeechRecognition || 
+    window.webkitSpeechRecognition;
+    const recognition = new window.SpeechRecognition();
+    recognition.lang = this.lang_;
+    recognition.interimResults = true;
+
+    // event current voice reco word
+    recognition.addEventListener("result", event => {      
+      var text = Array.from(event.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join("");
+      this.runtimeTranscription_ = text;
+    });
+    // end of transcription
+    recognition.addEventListener("end", () => {
+      this.transcription_.push(this.runtimeTranscription_);
+      this.runtimeTranscription_ = "";
+      recognition.stop();
+    });
+     recognition.start();
+   },
+
+   }
   }
-}
-</script>
+  </script>
