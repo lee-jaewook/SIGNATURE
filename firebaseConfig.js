dee2d0274a7d138ab59b978c6b8fccb562664739
lee-jaewook <jalee4930@gmail.com>
2021-05-30 20:43:04 +0900

    firebase config add

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyC0MRCLGaLNoyjDfMtsGVbHJav2v-GdnAM",
+    authDomain: "signature-snb.firebaseapp.com",
+    databaseURL: "https://signature-snb-default-rtdb.firebaseio.com",
+    projectId: "signature-snb",
+    storageBucket: "signature-snb.appspot.com",
+    messagingSenderId: "825551239574",
+    appId: "1:825551239574:web:4cfab39798e5faac3245b7"
+}
